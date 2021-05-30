1b3e954ee320a669d124ea238162757ffa9e9fe3
lila <m1339xt@gmail.com>
2021-05-30 17:18:15 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import routes from '@/router/pages';
 
 const router = createRouter({
+  base: process.env.NODE_ENV === 'production' ? '/hello-world' : '/',
   history: createWebHistory(),
   routes,
 });
