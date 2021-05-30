beed5d1e71228b2b4fe60c400a08422c0602ab0b
lila <m1339xt@gmail.com>
2021-05-30 15:21:03 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,4 @@
 <template>
-  <div>
-    <router-link to="/card">
-      card
-    </router-link>
-    <router-link to="/tic-tac-toe">
-      tic-tac-toe
-    </router-link>
-  </div>
-
   <router-view />
 </template>
 
