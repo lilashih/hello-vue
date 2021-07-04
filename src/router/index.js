d39e69cbb4081cdefca50f4923ddfee1cfb4b85a
lila <m1339xt@gmail.com>
2021-07-04 15:13:24 +0800

    add .env

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router
 import routes from '@/router/pages';
 
 const router = createRouter({
-  base: process.env.NODE_ENV === 'production' ? '/hello-world/' : '/',
+  base: import.meta.env.BASE_URL,
   // createWebHashHistory for github page
   history: process.env.NODE_ENV === 'production' ? createWebHashHistory() : createWebHistory(),
   routes,
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/hello-world' : '',
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, './src'),
-      },
-    ],
-  },
-  plugins: [vue()],
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    base: process.env.VITE_BASE_URL,
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: path.resolve(__dirname, './src'),
+        },
+      ],
+    },
+    plugins: [vue()],
+  });
+};
